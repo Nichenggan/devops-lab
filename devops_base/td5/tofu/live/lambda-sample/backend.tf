f3c15423fab2bb7e4ac82b67e5e1bff13b58a5a3
Chenyi Li <bracuslee@gmail.com>
2025-11-16 17:02:59 +0100

    test

--- devops_base/td5/tofu/live/lambda-sample/backend.tf
+++ devops_base/td5/tofu/live/lambda-sample/backend.tf
@@ -2,7 +2,7 @@ terraform {
 backend "s3" {
 bucket = "my-open-tofu-bucket-chenyi-cd" # Same bucket as above
 key = "td5/tofu/live/lambda-sample" # Unique key
-region = "eu-central-1" # Your AWS region
+region = "eu-central-1" 
 encrypt = true
 dynamodb_table = "my-open-tofu-bucket-chenyi-cd" # Same table as above
 }
